80d7070f339ebde027e8126b9037735abd95004f
Anthony Ahumada <locopala99@gmail.com>
2021-08-05 10:00:55 +0200

    more js

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -3,85 +3,85 @@ let preguntas2 = [
     pregunta: "Instrucción que permite almacenar un valor en una variable.",
     opcion: ["A.Leer", "B.Asignar", "C.Escribir", "D.Si / entonces"],
     respuesta: 2,
-    tip: "No lo se viejo me estoy llamando llama despues"
+    tip: "A esa clase si asisti es la B!"
   },
   {
     pregunta: "¿Para qué sirve git diff?",
     opcion: ["A.Lleva a cabo una función para establecer las diferencias en los orígenes de datos de Git. ", "B.Para revisar cuáles archivos se encuentran en el staged.", "Para inicializar un proyecto git.", "Git diff no es un comando de git."],
     respuesta: 1,
-    tip: "No lo se viejo me estoy llamando llama despues"
+    tip: "Lo siento amigo , en estos momentos voy conduciendo no te puedo contestar"
   },
   {
     pregunta: "¿ Que es bootstrap?",
     opcion: ["A. Biblioteca de herramientas ", "B. Pagina web ", "C. commando de HTML ", "D. Una organizacion "],
     respuesta: 1,
-    tip: "Viejo ese resident evil no lo jugue , pero creo que eran mas fuertes"
+    tip: "El telefono suena , pero nadie responde que mala suerte"
   },
   {
     pregunta: "Un bucle es una estructura:",
     opcion: ["A.Repetitiva", "B. Secuencial", "C. Condicional", "D.iterativa"],
     respuesta: 1,
-    tip: "Viejo ese resident evil no lo jugue , pero creo que eran mas fuertes"
+    tip: "Mmmm un blucle , me suena a algo que se repite , para mi esta entre la C y la A"
   },
   {
     pregunta: "Hay un terremoto, necesitas guardar la informacion",
     opcion: ["A. git (fetch pull commit)", "B. git (add log commit)", "C. git (push commit pull)", "D. git (add commit push)"],
     respuesta: 4,
-    tip: "Viejo ese resident evil no lo jugue , pero creo que eran mas fuertes"
+    tip: "Amigo soy abogado no programador , pero en mi hijo me dice que es la D"
   },
   {
-    pregunta: "Tipo de dato que permite almacenar cadenas de texto:",
+    pregunta: "Tipo de dato que permite almacenar cadenas de texto (En javascript):",
     opcion: ["A.int", "B.string", "C.char", "D.Java"],
     respuesta: 2,
-    tip:"Viejo creo que se transforma en el que tiene una lengua larga pero no estoy seguro",
+    tip:"No estoy seguro pero diria que es la C",
   },
   {
     pregunta: "¿Cómo puedo visualizar las ramas remotas en git?",
     opcion: ["A.Git branch -r", "B.Git branch", "C.Git branch origin remote", "D.Git checkout branch remote"],
     respuesta: 1,
-    tip:"Viejo creo que se transforma en el que tiene una lengua larga pero no estoy seguro",
+    tip:"Eso lo vimos en el seminario de FEDESOFT diria que es la A",
   },
   {
     pregunta: "¿Que hace un Scrum master?",
     opcion: ["A. Hace cumplir todo el ciclo SCRUM, ", "B. Se Encarga de las reuniones ", "C. Apoya a los otros miembros ", "D. Todas las anteriores"],
     respuesta: 4,
-    tip: "Viejo ese resident evil no lo jugue , pero creo que eran mas fuertes"
+    tip: "Yo soy ScrumMaster y hago de todo diria que es la D"
   },
   {
     pregunta: "Instrucción que permite emplear condiciones en un código de JavaScript:",
     opcion: ["A.I am inevitable", "si(condición){}", "C.if(condición){}", "D.while(condición){}"],
     respuesta: 3,
-    tip: "Viejo ese resident evil no lo jugue , pero creo que eran mas fuertes"
+    tip: "Esa si la vi en la universidad , es la C"
   },
   {
     pregunta: "Comando usado para para mostrar información sobre cualquier objeto git.",
     opcion: ["A.git stash", "B.git log", "C.git show", "D.git config --global"],
     respuesta: 3,
-    tip: "Viejo ese resident evil no lo jugue , pero creo que eran mas fuertes"
+    tip: "Uyyyy yo de Git si nulo , no te puedo ayudar lo siento"
   },
   {
     pregunta: "Las etapas que deben seguirse en la construcción de un programa son:",
     opcion: ["A.Análisis, algoritmo, diagrama de flujo, seudocódigo", "B.Análisis, seudocódigo, prueba de escritorio, implementación (codificar en un lenguaje de programación).", "C.Análisis, algoritmo, prueba de escritorio, implementación (codificar en un lenguaje de programación), prueba, instalación y uso del aplicativo.", "D.Análisis, algoritmo, diagrama de flujo, seudocódigo, prueba de escritorio, implementación (codificar en un lenguaje de programación), prueba, instalación y uso del aplicativo"],
     respuesta: 4,
-    tip: "Viejo ese resident evil no lo jugue , pero creo que eran mas fuertes"
+    tip: "Hablas con el indicado , es la D va a la fija!"
   },
   {
     pregunta: "En caso de presentarse una falla de sintaxis, en el proceso de programación, se procede a:",
     opcion: ["A.Ignorar los errores y ejecutar el programa.", "B.Comprender el mensaje de error que reporta el ambiente de programación, examinar el código del programa para identificar en cuál instrucción se encuentra la falla, corregir la falla, probar el programa de nuevo.", "C.Examinar el código del programa para identificar en cuál instrucción se encuentra la falla, Comprender el mensaje de error que reporta el ambiente de programación.", "D.Examinar el código del programa para identificar en cuál instrucción se encuentra la falla, corregir la falla."],
     respuesta: 3,
-    tip: "Viejo ese resident evil no lo jugue , pero creo que eran mas fuertes"
+    tip: "Mmmm Esa es simple es la C , yo vere me comparte el premio!"
   },
   {
     pregunta: "Comando empleado para extraer y descargar contenido desde un repositorio remoto y actualizar al instante el repositorio local para reflejar ese contenido.",
     opcion: ["A.git stash pop", "B.git rebase", "C.git status", "D.git pull"],
     respuesta: 4,
-    tip: "Viejo ese resident evil no lo jugue , pero creo que eran mas fuertes"
+    tip: "Mmmmm estoy confundido pero diria que esta entre la C y la D"
   },
   {
     pregunta: "Git es un sistema de control de versiones distribuido que fue creado por Linus Torvalds en el año:",
     opcion: ["A.2000", "B.2005", "C.2015", "D.1995"],
     respuesta: 2,
-    tip: "Viejo ese resident evil no lo jugue , pero creo que eran mas fuertes"
+    tip: "Uyyyy no me acuerdo ni el cumple de mis hijos menos de la fecha exacta de la creacion de Git , lo siento"
   },
 ];
 
@@ -295,7 +295,8 @@ function botoncincuenta(opcion) {
 }
 
 llamada.addEventListener('click', () => {
-
+  
+  if (span1.classList.contains("correcto") || span2.classList.contains("correcto") || span3.classList.contains("correcto") || span4.classList.contains("correcto")) return alert("UPS parece que un zombie te ha mordido , no puedes pedir el comodin pues ya tienes una opcion correcta")
   while (repetir3) repetir3 = false, alert(preguntas[index].tip);
 
 });
